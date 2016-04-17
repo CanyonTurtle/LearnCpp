7d470d12704fe16eacd69062cf7ed65bce611136
C A T <cannonator007@gmail.com>
2016-04-16 18:10:13 -0700

    Setup of solution with  first project.

---
+++ First/First.cpp
@@ -0,0 +1,17 @@
+// First.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Please enter an integer value: ";
+	int x;
+	std::cin >> x;
+	std::cout << "You have entered a change. " << x << std::endl;
+    return 0;
+}
+
+
