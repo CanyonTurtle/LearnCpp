20021bca8e6125e203fffb3cba1f0de6def7ded0
C A T <cannonator007@gmail.com>
2016-04-16 20:04:06 -0700

    added functions.

--- PrintWithParams/PrintWithParams.cpp
+++ PrintWithParams/PrintWithParams.cpp
@@ -9,6 +9,12 @@ void p(int toprint) {
 	std::cout << toprint << std::endl;
 }
 
+//returns sum of 2 inputted integers passed to the function as arguements.
+int addTwoInts(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
 	//passing 5 and 7 as arguements to p(). 
@@ -16,6 +22,7 @@ int main()
 	//"arguements are values passed from the caller to the function."
 	p(5);
 	p(7);
+	p(addTwoInts(5, 3));
     return 0;
 }
 
