ffba7bc74f85f3b45372441da62853f52a6c16c0
C A T <cannonator007@gmail.com>
2016-04-16 19:33:16 -0700

    completed the second console program.

--- SecondFunctionTest/SecondFunctionTest.cpp
+++ SecondFunctionTest/SecondFunctionTest.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+//the number that the computer was thinking of.
+int val()
+{
+	return 5;
+}
+
+//Gets user input and tells user what the computer was thinking.
+int interact_w_console()
+{
+	using std::cout;
+	using std::cin;
+	using std::endl;
+	cout << "Whats your charictar bro: ";
+	char response;
+	cin >> response;
+	cout << "Your response was " << response << endl;
+	cout << "I was thinking of the number " << val()<< endl;
+	return 0;
+}
 
 int main()
 {
+	interact_w_console();
     return 0;
 }
 
