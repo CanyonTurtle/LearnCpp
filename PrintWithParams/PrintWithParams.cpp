397ced5e91299641219f1bead23f14f3009866d4
C A T <cannonator007@gmail.com>
2016-04-16 19:59:02 -0700

    Printing with paramaters.

---
+++ PrintWithParams/PrintWithParams.cpp
@@ -0,0 +1,20 @@
+// PrintWithParams.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+//functon has one paramater.
+void p(int toprint) {
+	std::cout << toprint << std::endl;
+}
+
+int main()
+{
+	//passing 5 and 7 as arguements to p(). 
+	//paramaters are assigned the values of arguements passed to the called function.
+	p(5);
+	p(7);
+    return 0;
+}
+
